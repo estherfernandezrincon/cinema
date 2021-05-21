986ed1b783dd657d994075916585729642713c93
estherfernandezrincon <73235763+estherfernandezrincon@users.noreply.github.com>
2021-05-21 14:24:32 +0200

    mejorando

---
+++ cine.js
@@ -0,0 +1,16 @@
+let titulos = {
+    Mayo......nesa!! = "Oficial y Caballero";
+    A dios pongo por testigo que nunca jamás volveré a pasar hambre!! = "Lo que el viento se llevó";
+    Me llamo Iñigo Montoya, tu mataste a mi padre, preparate a morir = "La princesa prometida";
+    La tía Etna cuidará de Jack Jack = "Los increibles 2";
+    No dejaré que nadie te arrincone = "Dirty dancing";
+    Hasta el infinito....y más allá = "Toy story";
+    La fuerza de un verdadero Dios se mide por la fuerza del corazón = "Hércules";
+}
+
+if (input = titulos[input[Mayo...nesa!!]]){
+    alert('correcto')
+
+}else {
+    alert('prueba otra vez');
+}
