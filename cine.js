8e03a9aada403b40c3f6648cb2568eae5585388d
estherfernandezrincon <73235763+estherfernandezrincon@users.noreply.github.com>
2021-05-30 16:56:06 +0200

    cambios

--- cine.js
+++ cine.js
@@ -1,10 +1,10 @@
 peliculas= JSON.parse(frasesPeliculas)
 
 
-function elJuego() {
+function elJuego(id) {
     var right= "acierto";
     var wrong = "prueba otra vez";
-    var peli = document.getElementById("1");
+    var peli = document.getElementById(id);
 
     if(peli == peliculas[id][titulo])
         document.getElementById("message").innerHTML = right;
